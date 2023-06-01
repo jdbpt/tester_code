7b5af538464e7e24db74fd5d91ee841bb50aceb8
jdbpt <jbrown5.filn@gmail.com>
2023-06-01 15:11:16 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// let x = 15;
+let y = "yes";
+let z= "no";
+
+
+// const midgar = ["Cloud", "Tifa", "Yuffie", "Vincent Valentine"];
+// let names = "";
+
+// midgar.forEach(character => {
+//     names += character+" ";
+//     console.log(character);
+// });
+
+// console.log(names);
+
+// if(midgar.includes("Cloud")){
+//     console.log("Sephiroth attacks");
+// }
+
+let x = 42;
+let a = 70;
+
+if(x === 42  && a === 70){
+    let sum = x + a;
+    console.log(sum);
+}
+
+console.log(x, y, z);
+
+
